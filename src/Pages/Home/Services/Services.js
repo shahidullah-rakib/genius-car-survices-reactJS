dba9bcf7e42961ad10e2d3452971dc5ffd93278a
shsidulla <shsidullarakib@gmail.com>
2022-08-11 13:55:58 +0600

    add responsive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import repier from '../../../images/repier.png'
-
-const services = [
-    { id: 1, name: 'oil change', price: 100, description: '', img: 'repier' },
-    { id: 1, name: 'oil change', price: 100, description: '', img: 'repier' }
-];
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
+    const [services, setservices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setservices(data));
+    }, [])
     return (
         <div>
-            <h2>Services: </h2>
+            <h2 className='services-title'> Our Services</h2>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service key={service.id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
